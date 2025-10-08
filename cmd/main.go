0795e7102df0312556cbd73cf321294f8b5ecb79
Danil Bulgakov <gari-poter-2013-poter@mail.ru>
2025-10-08 18:43:52 +0300

    implement file upload service

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	//Создаем переменную типа log.Logger
+	var logger *log.Logger
+
+	//Создаем сервер с помощью функции Router из пакета server
+	server := server.Router(logger)
+
+	//Запускаем сервер
+	err := server.Server.ListenAndServe()
+	if err != nil {
+		//В случае ошибки запуска сервера логируем и завершаем приложение
+		log.Fatal(err)
+	}
 }
